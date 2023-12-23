823041caa1e510d2accfacadb18e85d1a35959d2
anonim00-hack <m65334287@gmail.com>
2023-12-23 13:56:04 +0500

    upgrade

---
+++ script.js
@@ -0,0 +1,60 @@
+const elbox = document.querySelector('.Necessary-sites-box');
+const elbright = document.querySelector('.brightness');
+const elbut = document.querySelector('.button-menu');
+const elbutclose = document.querySelector('.close');
+const elbutall = document.querySelector('.all');
+const elbutprog = document.querySelector('.programmers');
+const elbutfun = document.querySelector('.fun');
+const elbutaudio = document.querySelector('.type-audio');
+const elsites = document.querySelectorAll('.site');
+const elfunsites = document.querySelectorAll('.fun-site');
+const elprogsites = document.querySelectorAll('.programmers-site');
+const elaudio = document.querySelectorAll('.audio');
+const ellinebox = document.querySelector('.line-box');
+
+elbut.addEventListener('click', () => {
+    elbox.classList.add('visible');
+    elbright.classList.add('bright');
+});
+
+elbutclose.addEventListener('click', () => {
+    elbox.classList.remove('visible');
+    elbright.classList.remove('bright');
+});
+
+elbutprog.addEventListener('click', () => {
+    elfunsites.forEach(funsite => {
+        funsite.classList.add('hidden');
+    });
+    elprogsites.forEach(progsite => {
+        progsite.classList.remove('hidden');
+    });
+});
+elbutfun.addEventListener('click', () => {
+    elprogsites.forEach(prog => {
+        prog.classList.add('hidden');
+    });
+    elfunsites.forEach(funsite => {
+        funsite.classList.remove('hidden');
+    });
+});
+elbutaudio.addEventListener('click', () => {
+    elaudio.forEach(audio => {
+        audio.classList.toggle('hidden');
+    });
+    ellinebox.classList.toggle('an-hidden')
+});
+elbutall.addEventListener('click', () => {
+    elfunsites.forEach(funsite => {
+        funsite.classList.remove('hidden');
+    });
+    elprogsites.forEach(progsite => {
+        progsite.classList.remove('hidden');
+    });
+});
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Q' || event.key === 'q' || event.key === 'Й' || event.key === 'й') {
+        elbox.classList.toggle('visible');
+        elbright.classList.toggle('bright');
+    }
+});
